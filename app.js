7f932fe0e502e4be600c5ea1a30b54b4e7012a1c
ViihMarangoni <vmarangoni113@icloud.com>
2023-09-05 21:56:39 -0300

    calculo de méida

---
+++ app.js
@@ -0,0 +1,22 @@
+// comando de saída de dados - tudo que tiver a barra eu posso fazer uma anotação que não aparece
+/*
+alert("Oi, isso é uma mensagem") // saída
+
+console.log("Olá, isso é outra mensagem")
+
+alert(10/5) // fazer a conta aparecer no alert
+
+console.log(10/5) // fazer a conta aparecer no console
+*/
+// = é um operador de atribuição - ele pega de um lado e coloca do outro. 
+nota1 = 10
+nota2 = 8
+nota3 = 5
+nota4 = 8
+
+console.log("A média do João é: ")
+// console.log((nota1 + nota2 + nota3 + nota4)/4)
+
+media = ((nota1 + nota2 + nota3 + nota4)/4)
+
+console.log(media)
